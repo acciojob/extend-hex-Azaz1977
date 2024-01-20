1cc7d7e53b1036200404831899a502bfbdd9730d
Azaz1977 <154549223+Azaz1977@users.noreply.github.com>
2024-01-20 17:02:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+let fullHexCode ='';
+	
+for(let i = 0; i < shortHex.length; i++)
+    {
+        const tempHex = shortHex[i];
+        if(tempHex != '#')
+        {
+        fullHexCode += tempHex + tempHex;    
+        }
+    }
+	return '#'+fullHexCode;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex))
